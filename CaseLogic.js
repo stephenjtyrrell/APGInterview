6c49b3a8a35efad07c433aa39aacc86bce80067e
stephenjtyrrell <stetyrrell132@hotmail.com>
2022-07-14 17:31:57 +0100

    Adding JavaScript file for Case
    Handles OnLoad function logic for the case - retrieves today's date and the First Response By date on the Case, if they are equal, then it prompts the user with a warning

---
+++ CaseLogic.js
@@ -0,0 +1,25 @@
+function onLoad (executionContext){
+    var formContext = executionContext.getFormContext();
+
+    //create new date to store todays date
+    var todaysDate = new Date();
+    var dd = String(todaysDate.getDate()).padStart(2, '0');
+    var mm = String(todaysDate.getMonth() + 1).padStart(2, '0');
+    var yyyy = todaysDate.getFullYear();
+    todaysDate = dd + '/' + mm + '/' + yyyy;
+
+    //retrieve First Response By date from the Case form
+    var responseByDate = formContext.getAttribute("responseby").getValue();
+    var dd2 = String(responseByDate.getDate()).padStart(2, '0');
+    var mm2 = String(responseByDate.getMonth() + 1).padStart(2, '0');
+    var yyyy2 = responseByDate.getFullYear();
+    //stripping out Time from DateTime 
+    duebyDate =  dd2 + '/' + mm2 + '/' + yyyy2;
+
+    if(duebyDate == todaysDate){
+    formContext.ui.setFormNotification("This Case is Due Today", "WARNING", "1");
+    }
+    else{
+    formContext.ui.clearFormNotification("1")
+    }
+}
