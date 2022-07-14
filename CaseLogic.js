e43c9d82eae0cb634f6228999f2619624892b619
stephenjtyrrell <stetyrrell132@hotmail.com>
2022-07-14 18:40:14 +0100

    Adding null check
    Adding null check to First Response By, as it will always be null on create

--- CaseLogic.js
+++ CaseLogic.js
@@ -9,12 +9,15 @@ function onLoad (executionContext){
     todaysDate = dd + '/' + mm + '/' + yyyy;
 
     //retrieve First Response By date from the Case form
+    var duebyDate = new Date();
     var responseByDate = formContext.getAttribute("responseby").getValue();
+    if(responseByDate != null){
     var dd2 = String(responseByDate.getDate()).padStart(2, '0');
     var mm2 = String(responseByDate.getMonth() + 1).padStart(2, '0');
     var yyyy2 = responseByDate.getFullYear();
     //stripping out Time from DateTime 
     duebyDate =  dd2 + '/' + mm2 + '/' + yyyy2;
+    }
 
     if(duebyDate == todaysDate){
     formContext.ui.setFormNotification("This Case is Due Today", "WARNING", "1");
